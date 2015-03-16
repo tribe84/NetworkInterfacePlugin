8aedf00f8e4e1021087436e1756d1c8405543770
tribe84 <tribe84@gmail.com>
2015-03-16 13:14:12 -0700

    GetRouterAddress

--- www/networkinterface.js
+++ www/networkinterface.js
@@ -5,7 +5,7 @@ networkinterface.getIPAddress = function(success, fail) {
     cordova.exec(success, fail, "networkinterface", "getIPAddress", []);
 };
 
-networkinterface.getIPAddress = function(success, fail) {
+networkinterface.getRouterAddress = function(success, fail) {
     cordova.exec(success, fail, "networkinterface", "getRouterAddress", []);
 };
 
